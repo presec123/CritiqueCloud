30220e61ef25f4d9f8124cf933a082d92ff1b1dc
Louis <louisnokes3@gmail.com>
2023-11-12 15:15:04 +0000

    Added a footer + search bar to header component
    Also added a logo

--- CC-Frontend/src/App.tsx
+++ CC-Frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import Header from './components/header';
+import Header from './components/header/header';
+import Footer from './components/footer/footer';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Router>
       <Header />
+      <Footer />
       </Router>
     </div>
   );
