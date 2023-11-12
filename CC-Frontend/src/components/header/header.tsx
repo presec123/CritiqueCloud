24d554e978ec7636ef830950042569c8005bf14c
Louis <louisnokes3@gmail.com>
2023-11-12 15:20:55 +0000

    Update header.tsx

--- CC-Frontend/src/components/header/header.tsx
+++ CC-Frontend/src/components/header/header.tsx
@@ -7,34 +7,35 @@ const Header: React.FC = () => {
     return <div>
         <AppBar position="static">
             <Toolbar sx={{backgroundColor: '#222120', px: 3}}>
-                <Box sx={{ display: 'flex', alignItems: 'center', flexGrow: 5 }}>
-                    <img src={logo} alt="CC Logo" style={{ width: 50, height: 50, marginRight: 2 }}/>
-                    <Typography variant="h6" component="div">
-                        CritiqueCloud
-                    </Typography>
-                </Box>
-                <Box sx={{ display: 'flex', alignItems: 'center', flexGrow: 1 }}>
-                    <Button color="inherit" component={Link} to="/signin">Sign In</Button>
-                    <Button color="inherit" component={Link} to="/signup">Sign Up</Button>
-                    <Button color="inherit" component={Link} to="/categories">Categories</Button>
-                    <TextField 
-                        variant="outlined" 
-                        size="small" 
-                        placeholder="Search" 
-                        sx={{ 
-                            ml: 2, 
-                            width: '15ch',
-                            '& .MuiOutlinedInput-root': {
-                                borderRadius: '15px',
-                            },
-                            backgroundColor: 'white',
-                            borderRadius: '15px'
-                        }} 
-                    />
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                        <img src={logo} alt="CC Logo" style={{ width: 50, height: 50, marginRight: 2 }}/>
+                        <Typography variant="h6" component="div">
+                            CritiqueCloud
+                        </Typography>
+                    </Box>
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                        <Button color="inherit" component={Link} to="/signin">Sign In</Button>
+                        <Button color="inherit" component={Link} to="/signup">Sign Up</Button>
+                        <Button color="inherit" component={Link} to="/categories">Categories</Button>
+                        <TextField 
+                            variant="outlined" 
+                            size="small" 
+                            placeholder="Search" 
+                            sx={{ 
+                                ml: 2, 
+                                width: '15ch',
+                                '& .MuiOutlinedInput-root': {
+                                    borderRadius: '15px',
+                                },
+                                backgroundColor: 'white',
+                                borderRadius: '15px'
+                            }} 
+                        />
+                    </Box>
                 </Box>
             </Toolbar>
         </AppBar>
     </div>;
 }
-
 export default Header;
